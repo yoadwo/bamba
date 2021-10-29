c9085ac3b2a9c8905f75937ee4e298319d7a3416
yoadwo <yoadw20@gmail.com>
2021-10-30 00:51:27 +0300

    fix: base url

--- bamba-admin-pwa/src/environments/environment.prod.ts
+++ bamba-admin-pwa/src/environments/environment.prod.ts
@@ -7,6 +7,6 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'https://05fc-77-125-32-61.ngrok.io' + 'api/actions'
-  
+  baseUrl: 'https://05fc-77-125-32-61.ngrok.io' + '/api/actions'
+
 };
