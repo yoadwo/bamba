21cc7d3f7468236c39b35007fdf41df06f2f1879
yoadwo <yoadw20@gmail.com>
2021-11-03 18:45:30 +0200

    add: disabled effect for preview

--- bamba-admin-pwa/src/app/components/search/search.component.ts
+++ bamba-admin-pwa/src/app/components/search/search.component.ts
@@ -18,44 +18,50 @@ export class SearchComponent implements OnInit {
   displayedColumns: string[] = ['launch', 'preview', 'title'];
   voiceCommandsTable = new MatTableDataSource<VoiceCommand>();
   audioPreview: any;
+  isVoiceCommandPlaying: boolean;
   isTableValid: boolean;
 
   constructor(private voiceCommandsHttp: VoiceCommandsHttpService) {
     this.isTableValid = true;
-  }
+    this.isVoiceCommandPlaying = false;
+   }
 
   ngOnInit() {
     this.voiceCommandsHttp.getAllVoiceCommands().
-      then(results => {
-        this.isTableValid = true;
-        this.voiceCommandsTable.data = results;
-      })
-      .catch(err => {
-        this.isTableValid = false;
-        console.error(err);
-        this.voiceCommandsTable.data = [];
-
-      })
+    then(results => {
+      this.isTableValid = true;
+      this.voiceCommandsTable.data = results;
+    })
+    .catch(err => {
+      this.isTableValid = false;
+      console.error(err);
+      this.voiceCommandsTable.data = [];
+      
+    })
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
-  activateVoiceCommand(id: number) {
+  activateVoiceCommand(id: number) {    
     this.voiceCommandsHttp.activateVoiceCommand(id);
   }
 
-  PlayAudioPreview(id: number) {
+  playAudioPreview(voiceCommand: any){
     // single audio element in DOM, change its src only if a different audio
-    if (this.audioPlayerRef.nativeElement.getAttribute('audio-id') != id) {
-      this.audioPlayerRef.nativeElement.setAttribute('src', this.GetAudioPreviewFile(id));
-      this.audioPlayerRef.nativeElement.setAttribute('audio-id', id);
+    if (this.audioPlayerRef.nativeElement.getAttribute('audio-id') != voiceCommand.id){
+      this.audioPlayerRef.nativeElement.setAttribute('src',this.getAudioPreviewFile(voiceCommand.id));
+      this.audioPlayerRef.nativeElement.setAttribute('audio-id', voiceCommand.id);
     }
+    voiceCommand.isPlaying = true;
     this.audioPlayerRef.nativeElement.play();
+    this.audioPlayerRef.nativeElement.addEventListener('ended', () => {
+      voiceCommand.isPlaying = false;
+    });
   }
 
-  GetAudioPreviewFile(id: number) {
+  getAudioPreviewFile(id: number){  
     return this.voiceCommandsHttp.getAudioPreview(id);
   }
 }
